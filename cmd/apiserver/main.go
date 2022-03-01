3d785297527761b502c38bad8850c2c8f87b1187
AlyonaAg <gigala2001@gmail.com>
2022-03-01 20:17:53 +0300

    Init

---
+++ cmd/apiserver/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"BIP_backend/internal/app/apiserver"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("main")
+	apiserver.Print()
+	apiserver.Print2()
+}
---
+++ internal/app/apiserver/config.go
@@ -0,0 +1,11 @@
+package apiserver
+
+import "fmt"
+
+func Print() {
+	fmt.Println("config")
+}
+
+func Print2() {
+	fmt.Println("aa")
+}
