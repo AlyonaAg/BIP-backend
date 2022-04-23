c72cf8129fd4b90603855975540a946ca70feb51
AlyonaAg <gigala2001@gmail.com>
2022-04-23 20:45:38 +0300

    address fix

--- docs/docs.go
+++ docs/docs.go
@@ -1416,7 +1416,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "51.250.18.28:8080",
+	Host:             "51.250.22.74:8080",
 	BasePath:         "/api",
 	Schemes:          []string{},
 	Title:            "BIP API",
